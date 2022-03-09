ea7f6afbcaae5b892af2eacd1cd9add4d27fb831
saikinkatenai95 <saikinkatenai95@outlook.jp>
2022-03-09 17:53:24 +0900

    連結判定メソッド

--- linking.rb
+++ linking.rb
@@ -1,10 +1,16 @@
-puts "連結を判定"
-n, m = gets.split.map(&:to_i)
+puts "頂点の数を入力"
+n = gets.to_i
+puts "辺の数を入力"
+m = gets.to_i
 
 
 g = Array.new(n).map { Array.new(n, 0) }
-m.times do
-  a, b = gets.split.map(&:to_i)
+puts "#{m}回入力"
+m.times do |i|
+  puts "#{i+1}回目"
+  puts "両端の頂点の数を入力"
+  a = gets.to_i
+  b = gets.to_i
   g[a - 1][b - 1] = 1
   g[b - 1][a - 1] = 1
 end
